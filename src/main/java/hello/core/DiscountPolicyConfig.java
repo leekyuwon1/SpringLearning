7d734aab691051e4bc1d01221c47bbbe52d37a2d
leekyuwon <sjin9574@naver.com>
2024-01-28 20:58:44 +0900

    240125

---
+++ src/main/java/hello/core/DiscountPolicyConfig.java
@@ -0,0 +1,22 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// 수동으로 빈으로 등록.
+@Configuration
+public class DiscountPolicyConfig {
+
+    @Bean
+    public DiscountPolicy rateDiscountPolicy() {
+        return new RateDiscountPolicy();
+    }
+
+    @Bean
+    public DiscountPolicy fixDiscountPolicy() {
+        return new FixDiscountPolicy();
+    }
+}
