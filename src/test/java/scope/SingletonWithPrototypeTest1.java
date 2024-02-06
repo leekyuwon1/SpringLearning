35932a7e4f20c373b763958aefe226e300201ff6
leekyuwon <sjin9574@naver.com>
2024-02-07 00:13:50 +0900

    240207

--- src/test/java/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/scope/SingletonWithPrototypeTest1.java
@@ -4,6 +4,7 @@ import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -35,18 +36,24 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
+    @Scope("singleton")
     static class ClientBean{
-        private final PrototypeBean prototypeBean;
+//        private final PrototypeBean prototypeBean; // 생성 시점에 주입이 되어 있다.
+
+//        @Autowired
+//        public ClientBean(PrototypeBean prototypeBean) {
+//            this.prototypeBean = prototypeBean;
+//        }
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        ApplicationContext applicationContext;
+
 
         public int logic() {
+            PrototypeBean prototypeBean = applicationContext.getBean(PrototypeBean.class);
             prototypeBean.addCount();
             return prototypeBean.getCount();
 
